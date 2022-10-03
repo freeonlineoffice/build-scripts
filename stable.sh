bf633547c6e4ecc8747ec78616eb34482c296019
Marco Marinello <me@marcomarinello.it>
2022-10-03 22:36:32 +0200

    Set branch to 7-4 for stable builds

--- stable.sh
+++ stable.sh
@@ -2,7 +2,7 @@
 
 SRCDIR="$(realpath `dirname $0`)"
 
-export FREEONLINEOFFICE_BRANCH="docker"
+export FREEONLINEOFFICE_BRANCH="7-4"
 export CORE_BRANCH="libreoffice-7-4"
 export DOCKER_HUB_REPO="freeonlineoffice/online"
 export DOCKER_HUB_TAG="nightly"
