80e423764a513ac6e0dff6b24b71fe6a09b4c36f
Marco Marinello <me@marcomarinello.it>
2022-10-09 21:58:34 +0200

    Update stable build script: tag with BRANCH name

--- stable.sh
+++ stable.sh
@@ -4,7 +4,7 @@ SRCDIR="$(realpath `dirname $0`)"
 
 export FREEONLINEOFFICE_BRANCH="7-4"
 export CORE_BRANCH="libreoffice-7-4"
-export DOCKER_HUB_REPO="freeonlineoffice/online"
-export DOCKER_HUB_TAG="nightly"
+export DOCKER_HUB_REPO="ghcr.io/freeonlineoffice/online"
+export DOCKER_HUB_TAG="$FREEONLINEOFFICE_BRANCH"
 
 . $SRCDIR/run_build.sh
