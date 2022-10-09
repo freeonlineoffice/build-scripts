300de653c5babfcfb968aca8f1fe087f6b204514
Marco Marinello <me@marcomarinello.it>
2022-10-09 21:03:29 +0200

    Install node via nodesource to avoid errors Also add libgif-dev

--- debian10_builddeps.sh
+++ debian10_builddeps.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Install node from nodesource since newer versions give error
+[ ! -f /etc/apt/sources.list.d/nodesource.list ] && (
+  curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+  apt-get install -y nodejs
+)
+
 # From https://wiki.documentfoundation.org/Development/BuildingOnline#Prerequisites_for_building_on_Debian_11_.28Bullseye.29
-apt-get install -y git build-essential devscripts libkrb5-dev openssl g++ libssl-dev libcap-dev libtool m4 automake libcppunit-dev libcppunit-doc pkg-config python-polib python3-polib default-jre libpam-dev npm python-lxml python3-lxml translate-toolkit docker.io locales-all libpng16-16 fontconfig adduser cpio findutils nano libpoco-dev libcap2-bin openssl inotify-tools procps libubsan1 nasm
+apt-get install -y git build-essential devscripts libkrb5-dev openssl g++ libssl-dev libcap-dev libtool m4 automake \
+                   libcppunit-dev libcppunit-doc pkg-config python-polib python3-polib default-jre libpam-dev \
+                   python-lxml python3-lxml translate-toolkit docker.io locales-all libpng16-16 fontconfig adduser \
+                   cpio findutils nano libpoco-dev libcap2-bin openssl inotify-tools procps libubsan1 nasm libgif-dev
 apt-get build-dep -y libreoffice
-npm i npm@latest -g
 
 # Our extras
 apt install -qq -y rsync ccache
