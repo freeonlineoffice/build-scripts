9ba7a8f7b19edb9b8c1d2fed1c51837577a01044
andreasma <maand@gmx.de>
2022-11-26 21:35:02 +0100

    Use the master branch of online
    Signed-off-by: andreasma <maand@gmx.de>

--- nightly.sh
+++ nightly.sh
@@ -2,7 +2,7 @@
 
 SRCDIR="$(realpath `dirname $0`)"
 
-export FREEONLINEOFFICE_BRANCH="docker"
+export FREEONLINEOFFICE_BRANCH="master"
 export CORE_BRANCH="master"
 export DOCKER_HUB_REPO="freeonlineoffice/online"
 export DOCKER_HUB_TAG="nightly"
