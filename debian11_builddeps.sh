e0b45c97a765bc761f0559f0d9b2b6487364ef29
Marco Marinello <me@marcomarinello.it>
2022-12-28 14:13:09 +0100

    Added libzstd-dev to dependencies, install node 14 from nodesource also for Bullseye currently bugged, see https://github.com/nodejs/help/issues/3644

--- debian11_builddeps.sh
+++ debian11_builddeps.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Install node from nodesource since newer versions give error
+[ ! -f /etc/apt/sources.list.d/nodesource.list ] && (
+  curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
+)
+
 apt-get install -y git build-essential devscripts libkrb5-dev openssl g++ libssl-dev libcap-dev libtool m4 automake \
-                   libcppunit-dev libcppunit-doc pkg-config python3-polib python3-polib default-jre libpam-dev npm \
+                   libcppunit-dev libcppunit-doc pkg-config python3-polib python3-polib default-jre libpam-dev nodejs \
                    python3-lxml translate-toolkit docker.io locales-all libpng16-16 fontconfig adduser cpio findutils \
-                   nano libpoco-dev libcap2-bin openssl inotify-tools procps libubsan1 nasm libgif-dev
+                   nano libpoco-dev libcap2-bin openssl inotify-tools procps libubsan1 nasm libgif-dev libzstd-dev
 
 apt-get build-dep -y libreoffice
-npm i npm@latest -g
 
 # Our extras
 apt install -qq -y rsync ccache
