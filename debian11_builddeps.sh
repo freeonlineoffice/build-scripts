c6998eb57ae08b078809bf93c557d2a1c0f1f803
andreasma <maand@gmx.de>
2023-01-06 19:50:25 +0100

    add npm to the build dependencies
    Signed-off-by: andreasma <maand@gmx.de>

--- debian11_builddeps.sh
+++ debian11_builddeps.sh
@@ -4,7 +4,7 @@ apt-get install -y git build-essential devscripts libkrb5-dev openssl g++ libssl
                    libcppunit-dev libcppunit-doc pkg-config python3-polib python3-polib default-jre libpam-dev nodejs \
                    python3-lxml translate-toolkit docker.io locales-all libpng16-16 fontconfig adduser cpio findutils \
                    nano libpoco-dev libcap2-bin openssl inotify-tools procps libubsan1 nasm libgif-dev libzstd-dev \
-                   curl rsync libfontconfig1-dev gperf libnss3-dev bison flex zip
+                   curl rsync libfontconfig1-dev gperf libnss3-dev bison flex zip npm
 
 # Install node from nodesource since newer versions give error
 [ ! -f /etc/apt/sources.list.d/nodesource.list ] && (
