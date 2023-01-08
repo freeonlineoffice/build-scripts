db8a26f460d80955b730cbf2f989a65c379e49a8
Marco Marinello <me@marcomarinello.it>
2023-01-08 20:52:36 +0100

    Fix nodejs setup

--- debian11_builddeps.sh
+++ debian11_builddeps.sh
@@ -4,12 +4,14 @@ apt-get install -y git build-essential devscripts libkrb5-dev openssl g++ libssl
                    libcppunit-dev libcppunit-doc pkg-config python3-polib python3-polib default-jre libpam-dev nodejs \
                    python3-lxml translate-toolkit docker.io locales-all libpng16-16 fontconfig adduser cpio findutils \
                    nano libpoco-dev libcap2-bin openssl inotify-tools procps libubsan1 nasm libgif-dev libzstd-dev \
-                   curl rsync libfontconfig1-dev gperf libnss3-dev bison flex zip npm
+                   curl rsync libfontconfig1-dev gperf libnss3-dev bison flex zip
 
 # Install node from nodesource since newer versions give error
 [ ! -f /etc/apt/sources.list.d/nodesource.list ] && (
   curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
 )
+
+apt-get install -y nodejs
 
 apt-get build-dep -y libreoffice
 
