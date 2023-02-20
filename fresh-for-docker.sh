aba7819f1c72c6ea7e362fdc690e0c860f50e9d9
andreasma <maand@gmx.de>
2023-02-20 15:57:56 +0100

    Add a script to build version 7.5 for docker hub
    Signed-off-by: andreasma <maand@gmx.de>

---
+++ fresh-for-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SRCDIR="$(realpath `dirname $0`)"
+
+export FREEONLINEOFFICE_BRANCH="7-5"
+export CORE_BRANCH="libreoffice-7-5"
+export DOCKER_HUB_REPO="freeonlineoffice/online"
+export DOCKER_HUB_TAG="$FREEONLINEOFFICE_BRANCH"
+
+. $SRCDIR/run_build.sh
