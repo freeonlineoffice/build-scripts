8f1445f796e205a027e730a1f608b6cca31b27e9
andreasma <maand@gmx.de>
2024-03-23 19:27:58 +0100

    Create debian12_builddeps.sh

---
+++ debian12_builddeps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+apt-get install -y git build-essential devscripts libkrb5-dev openssl g++ libssl-dev libcap2-dev libtool m4 automake \
+                   libcppunit-dev libcppunit-doc pkg-config python3-polib python3-polib default-jre libpam-dev nodejs \
+                   python3-lxml translate-toolkit docker.io locales-all libpng16-16 fontconfig adduser cpio findutils \
+                   nano libpoco-dev libcap2-bin openssl inotify-tools procps libubsan1 nasm libgif-dev libzstd-dev \
+                   curl rsync libfontconfig1-dev gperf libnss3-dev bison flex zip inotify-tools openssh-client \
+		   fonts-wqy-zenhei fonts-wqy-microhei fonts-droid-fallback fonts-noto-cjk
+
+# Install node from nodesource since newer versions give error
+[ ! -f /etc/apt/sources.list.d/nodesource.list ] && (
+  curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
+)
+
+apt-get install -y nodejs
+
+apt-get build-dep -y libreoffice
+
+# Our extras
+apt install -qq -y rsync ccache
+
+
